1dc95411c1a36d86a64c089784b2c90a36b6f2b0
Musa <zuluchs@gmail.com>
2021-11-04 18:50:39 +0200

    added models

---
+++ source/CustomerListing.DB/Domain/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerListing.DB.Domain
+{
+    public class Customer
+    {
+        public Guid CustomerId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(10)]
+        public string Cellphone { get; set; }
+        public decimal AmountTotal { get; set; }
+        [Required]
+        public Type Type { get; set; }
+    }
+}
---
+++ source/CustomerListing.DB/Enums/Type.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomerListing.DB.Enums
+{
+    public enum Type
+    {
+        NEW_CUSTOMER = 1,
+        LOYAL_CUSTOMER = 2,
+        DISCOUNT_CUSTOMER = 3,
+        POTENTIAL_CUSTOMER = 4
+    }
+}
