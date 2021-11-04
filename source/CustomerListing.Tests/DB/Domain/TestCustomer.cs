b1feebcaf1cb67bbd8678427e0a8ea95a15e2b36
Musa <zuluchs@gmail.com>
2021-11-04 20:02:23 +0200

    Tests
    Added domain tests

---
+++ source/CustomerListing.Tests/DB/Domain/TestCustomer.cs
@@ -0,0 +1,39 @@
+﻿using CustomerListing.DB.Domain;
+using CustomerListing.DB.Enums;
+using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+using System;
+
+namespace CustomerListing.Tests.DB.Domain
+{
+    [TestFixture]
+    public class TestCustomer
+    {
+        [Test]
+        public void Construct()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            Assert.DoesNotThrow(() => new Customer());
+            //---------------Test Result -----------------------
+        }
+
+        [TestCase("CustomerId", typeof(Guid))]
+        [TestCase("FirstName", typeof(string))]
+        [TestCase("LastName", typeof(string))]
+        [TestCase("Email", typeof(string))]
+        [TestCase("Cellphone", typeof(string))]
+        [TestCase("AmountTotal", typeof(decimal))]
+        [TestCase("Type", typeof(CustomerType))]
+        public void Customer_ShouldHaveProperty(string propertyName, Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof(Customer);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            sut.ShouldHaveProperty(propertyName, propertyType);
+            //---------------Test Result -----------------------
+        }
+    }
+}
---
+++ source/CustomerListing.Tests/DB/Domain/TestEntityBase.cs
@@ -0,0 +1,39 @@
+﻿using CustomerListing.DB.Domain;
+using NUnit.Framework;
+using PeanutButter.TestUtils.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerListing.Tests.DB.Domain
+{
+    [TestFixture]
+    public class TestEntityBase
+    {
+        [Test]
+        public void Construct()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            Assert.DoesNotThrow(() => new EntityBase());
+            //---------------Test Result -----------------------
+        }
+
+        [TestCase("CreatedBy", typeof(string))]
+        [TestCase("LastUpdatedBy", typeof(string))]
+        [TestCase("DateCreated", typeof(DateTime?))]
+        [TestCase("DateLastModified", typeof(DateTime?))]
+        public void EntityBase_ShouldHaveProperty(string propertyName, Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof(EntityBase);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            sut.ShouldHaveProperty(propertyName, propertyType);
+            //---------------Test Result -----------------------
+        }
+    }
+}
