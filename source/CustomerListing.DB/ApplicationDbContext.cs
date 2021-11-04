148d3d62c635b823d97324148e42f9c411b3b8b2
Musa <zuluchs@gmail.com>
2021-11-04 18:54:46 +0200

    updated application db context

--- source/CustomerListing.DB/ApplicationDbContext.cs
+++ source/CustomerListing.DB/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CustomerListing.DB.Domain;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace CustomerListing.DB
 {
     public interface IApplicationDbContext
     {
+        DbSet<Customer> Customers { get; set; }
         Task<int> SaveChangesAsync();
     }
     public sealed class ApplicationDbContext : DbContext, IApplicationDbContext
@@ -13,11 +15,25 @@ namespace CustomerListing.DB
            : base(options)
         {
         }
+
+        public DbSet<Customer> Customers { get; set; }
         public Task<int> SaveChangesAsync() => base.SaveChangesAsync();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Customer>().ToTable("Customers");
+
+            builder.Entity<Customer>().Property(f => f.CustomerId).IsRequired();
+            builder.Entity<Customer>().Property(f => f.FirstName).IsRequired();
+            builder.Entity<Customer>().Property(f => f.LastName).IsRequired();
+            builder.Entity<Customer>().Property(f => f.Cellphone).HasMaxLength(10);
         }
     }
 }
